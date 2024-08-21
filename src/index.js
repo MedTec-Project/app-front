425e4ac94cafa0c64b1dfb370b2e61e7d9f62618
Ruhan <ruhankaio900@gmail.com>
2024-08-21 20:48:07 -0300

    The beginning of the login screen

--- src/index.js
+++ src/index.js
@@ -7,19 +7,25 @@ import {
 import "./index.scss";
 import LoginForm from "./views/login/LoginForm.jsx";
 
+const user = localStorage.getItem('user')
+console.log(user)
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Inicio /</div>,
-  },
+  }
+]);
+
+const login = createBrowserRouter([
   {
-    path: "/Login",
+    path: "/",
     element: <LoginForm />
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={user ? router : login} />
   </React.StrictMode>
 );
