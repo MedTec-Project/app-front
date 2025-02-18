475cee85d76f4d12a1db39a6d5120549aa3cac3a
NICOLAS BRUSKI <nicolas.bruski@alunos.sc.senac.br>
2025-02-17 21:35:07 -0300

    feature/menu: Menu de navegação para o usuário

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,9 @@
 import { Link } from 'react-router-dom';
 import './styles.css'
-import { useContext } from 'react';
-import { AuthContext } from '../../auth/Context';
 import SearchInput from '../SearchInput';
-import { FaRegUser } from "react-icons/fa";
+import Dropdown from '../Dropdown/index';
 
 export default function Header() {
-
-    const { token } = useContext(AuthContext);
-
     return (
         <header>
             <nav>
@@ -18,17 +13,10 @@ export default function Header() {
                     <Link to="/agendamentos">Agendamentos</Link>
                     <Link to="/calendario">Calendário</Link>
                 </div>
-                <div className="right"><SearchInput/><FaRegUser color='white' size={30}/></div>
-                {/* <Link to="/"><img src={logo} alt="Logo" className="header-logo" /></Link>
-                <ul>
-                    <Link to="/">
-                        <li>HOME</li>
-                    </Link>
-                    <Link to="/favorites"><li>Favoritos</li></Link>
-                    <Link to="/feed"><li>Feed</li></Link>
-                    {token && <Link to="/profile">Perfil</Link>}
-                    {token && <LogoutButton />}
-                </ul> */}
+                <div className="right">
+                <SearchInput/>
+                <Dropdown/>
+                </div>
             </nav>
         </header>
     )
