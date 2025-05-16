52bae522568286bc3830abe0a5ebb78b24715957
Richard Pereira Fernandes <richard.fernandes@moinformatica.com>
2025-05-16 13:22:45 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // permite acesso externo (fora do container)
+    port: 5173,       // porta padrão do Vite
+  }
 })
