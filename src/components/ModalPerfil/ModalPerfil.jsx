2dff5629f0c93d1b1cb1c9dbc15e67dab988583b
nicolas <nicolasbruski7@gmail.com>
2025-06-09 20:38:14 -0300

    feature/perfilModal: finalizado modal do perfil do usuário

--- src/components/ModalPerfil/ModalPerfil.jsx
+++ src/components/ModalPerfil/ModalPerfil.jsx
@@ -1,5 +1,6 @@
 import './ModalPerfil.css';
 import {AiOutlineClose} from "react-icons/ai";
+import ButtonComorbidade from '../ButtonComorbidade/buttonComorbidade';
 
 export default function ModalPerfil({modalOpen, handleClose}) {
     return(
@@ -13,20 +14,45 @@ export default function ModalPerfil({modalOpen, handleClose}) {
                 <div className='cnt-perfil'>
                     <div className='pos-info'>
                         <div className='pos-foto'>
-                            <button className='foto-perfil'></button>
+                            <div className='foto-perfil'>
+                                <div className='foto'>
+
+                                </div>
+                            </div>
                         </div>
                         <div className='pos-dados'>
-                            <h4>Mohamed Lee</h4>
+                            <h3>Mohamed Lee</h3>
                             <div className='pos-tit'>
-                                <p>CPF: xxx.xxx.xxx-xx</p>
+                                <p>CPF: XXX.XXX.XXX-XX</p>
                                 <p>Telefone: (47) 99783-9970</p>
                             </div>
                         </div>
-                    </div>
-                    
+                    </div>                   
                 </div>
                 <div className='cnt-button'>
                     <button className='but-edit'>Editar Perfil</button>
+                </div>
+            </div>
+            <div className='nav-modal'>
+                <div className='nav-itens'>
+                    <button className='iten-button'>Dados</button>
+                    <button className='iten-button'>Comorbidades</button>
+                    <button className='iten-button'>Responsáveis</button>
+                    <button className='iten-button'>Médicos</button>
+                </div>
+            </div>
+            <div className='cnt-iten'>
+                <div className='cnt-position'>
+                    <ButtonComorbidade />
+                    <ButtonComorbidade />
+                    <ButtonComorbidade />
+                    <ButtonComorbidade />
+                </div>
+            </div>
+            <div className='div-button'>
+                <div className='pos-button'>
+                    <button className='button-add'>Adicionar</button>
+                    <button className='button-edt'>Editar</button>
                 </div>
             </div>
         </div>
