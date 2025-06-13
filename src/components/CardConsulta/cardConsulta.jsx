043d5b5606e8959e11a9a0f087819a0d862b6ee3
nicolas <nicolasbruski7@gmail.com>
2025-06-12 22:03:02 -0300

    feat(agendamentoConsulta): finalizado o card

--- src/components/CardConsulta/cardConsulta.jsx
+++ src/components/CardConsulta/cardConsulta.jsx
@@ -1,9 +1,24 @@
 import '../CardConsulta/cardConsulta.css';
+import image from '../../assets/images/doctor.png';
 
 export default function CardConsulta() {
-    <div className='card-main'>
-        <div className='pos-foto'>
-            <h1>dfsdfsd</h1>
+    return(
+        <div className='card-main'>
+            <div style={{margin: "40px"}}>
+                <div className='pos-foto'>
+                    <div className='foto'>
+                        <img src={image} alt="Foto de perfil" className="foto-perfil" />
+                    </div>
+                </div>
+                <div className='tittle'>
+                    <label>Médico: Maicon Douglas</label>
+                </div>
+                <div className='pos-information'>
+                    <h4 style={{textAlign: "center",paddingBottom: "10px", fontWeight: "100"}}>INFORMAÇÕES</h4>
+                    <label>Horário: 15:00h</label><br />
+                    <label>Data: 12/12/2024</label>
+                </div>
+            </div>
         </div>
-    </div>
+    )
 }
