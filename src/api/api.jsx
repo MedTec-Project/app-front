bb9ab1b0ca2c9ef69574e2ae8b60318a798c6a1d
RichardPFernandes <fernandesrichard312@gmail.com>
2025-07-01 20:46:55 -0300

    quase finalizado

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 2 - Adicionar Axios e criar uma instância do Axios
 const api = axios.create({
   baseURL: 'https://app-back-ye0w.onrender.com/',
-  timeout: 10000,
+  timeout: 50000,
 });
 
 api.interceptors.request.use(
